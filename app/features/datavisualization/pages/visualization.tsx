921e3e941b5d46a12682eb1e63880a0e7076e903
진승연 <jins4731@gmail.co.kr>
2025-12-08 20:19:53 +0900

    add: flex-layout

--- app/features/datavisualization/pages/visualization.tsx
+++ app/features/datavisualization/pages/visualization.tsx
@@ -1,27 +1,73 @@
-import { Minus, Plus } from "lucide-react";
-import React from "react";
-
-import { Button } from "~/common/components/ui/button";
-import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "~/common/components/ui/drawer";
+import React, { useState } from "react";
+import ItemGroup from '../items/itemGroup';
+import Container from "~/common/components/ui/container";
+import {Layout, Model} from 'flexlayout-react';
+import "flexlayout-react/style/light.css";  
+import "./visualization.css";
 
 const visualization = () => {
-  const [goal, setGoal] = React.useState(350)
-  function onClick(adjustment: number) {
-    setGoal(Math.max(200, Math.min(400, goal + adjustment)))
+  const json = {
+      global: {},
+      borders: [],
+      layout: {
+          type: "row",
+          weight: 100,
+          children: [
+              {
+                  type: "tabset",
+                  weight: 50,
+                  children: [
+                      {
+                          className:'item1',
+                          type: "tab",
+                          name: "Chart 1",
+                          component: "placeholder",
+                          // component: defaultItem || 'chart'
+                      },
+                      {
+                          className:'item2',
+                          type: "tab",
+                          name: "Chart 2",
+                          component: "placeholder",
+                          // component: defaultItem || 'chart'
+                      }
+                  ]
+              },
+              {
+                  type: "tabset",
+                  weight: 50,
+                  children: [
+                      {
+                          type: "tab",
+                          name: "Two",
+                          component: "placeholder",
+                      }
+                  ]
+              }
+          ]
+      }
+  };
+  const model = Model.fromJson(json);
+  const [items, setItems] = useState();
+  
+  const factory = (node: any) => {
+    const component = node.getComponent();
+
+    if (component === "placeholder") {
+      return <div>{node.getName()}</div>;
+    }
   }
 
   return (
-    <>
-      <div>visualization page</div>
+      <div className="flex flex-col h-screen py-2 px-2">
+        <div className="h-[10%]">
+          <ItemGroup />
+        </div>
 
-      <Drawer direction="left">
-        <DrawerTrigger asChild>
-          <Button variant="outline">Open DataTable</Button>
-        </DrawerTrigger>
-        <DrawerContent>
-        </DrawerContent>
-      </Drawer>
-    </>
+        <div className="h-[90%] border-2">
+          <Layout model={model} factory={factory} />
+        </div>
+      </div>
   )
 }
 
